0aa40291ce4a616a087459cd32e6edc5e2112fc3
Sofyan Ajridi <sofyan.ajridi@vub.be>
2023-02-09 18:06:36 +0100

    Updated Powergym to support Gymnasium

--- experiments/powergym/powergym_random_agent.py
+++ experiments/powergym/powergym_random_agent.py
@@ -2,7 +2,7 @@ from powergym.powergym.env_register import make_env
 import matplotlib.pyplot as plt
 
 NR_EPISODES = 1000
-env = make_env('13Bus')
+env = make_env('13Bus', multi_objective=False)
 
 r_per_episode = [0] * (NR_EPISODES)
 for i in range(1, NR_EPISODES):
@@ -10,11 +10,7 @@ for i in range(1, NR_EPISODES):
     done = False
     while not done:
         action = env.action_space.sample()
-        obs, reward, done, info = env.step(action)
-        r_per_episode[i] += reward
+        obs, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
 
 
-cum_r_per_episode = [item / (idx + 1) for idx,item in enumerate(r_per_episode)]
-print(cum_r_per_episode)
-plt.plot(r_per_episode)
-plt.show()
