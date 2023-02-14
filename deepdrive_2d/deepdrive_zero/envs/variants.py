5925ef57e88e61c2865738296eb769675018fc83
Sofyan Ajridi <sofyan.ajridi@vub.be>
2023-02-14 12:13:40 +0100

    Deepdrive: render_mode is now optional

--- deepdrive_2d/deepdrive_zero/envs/variants.py
+++ deepdrive_2d/deepdrive_zero/envs/variants.py
@@ -2,33 +2,33 @@ from deepdrive_2d.deepdrive_zero.envs import Deepdrive2DEnv
 
 
 class OneWaypointSteerOnlyEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration,  render_mode=None):
         super().__init__(is_one_waypoint_map=True, match_angle_only=True, env_configuration=env_configuration,
                          render_mode=render_mode)
 
 
 class OneWaypointEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration,  render_mode=None):
         super().__init__(is_one_waypoint_map=True, match_angle_only=False, env_configuration=env_configuration,
                          render_mode=render_mode)
 
 
 class IncentArrivalEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration,  render_mode=None):
         super().__init__(is_one_waypoint_map=True, match_angle_only=False,
                          incent_win=True, env_configuration=env_configuration,
                          render_mode=render_mode)
 
 
 class StaticObstacleEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration,  render_mode=None):
         super().__init__(is_one_waypoint_map=True, match_angle_only=False,
                          incent_win=True, add_static_obstacle=True, env_configuration=env_configuration,
                          render_mode=render_mode)
 
 
 class NoGforcePenaltyEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration,  render_mode=None):
         super().__init__(is_one_waypoint_map=True, match_angle_only=False,
                          incent_win=True, add_static_obstacle=True,
                          disable_gforce_penalty=True, env_configuration=env_configuration,
@@ -36,7 +36,7 @@ class NoGforcePenaltyEnv(Deepdrive2DEnv):
 
 
 class SixtyFpsEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration,  render_mode=None):
         super().__init__(is_one_waypoint_map=True, match_angle_only=False,
                          incent_win=True, add_static_obstacle=True,
                          disable_gforce_penalty=True,
@@ -45,21 +45,21 @@ class SixtyFpsEnv(Deepdrive2DEnv):
 
 
 class IntersectionEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration,  render_mode=None):
         super().__init__(is_intersection_map=True, match_angle_only=False,
                          incent_win=True, disable_gforce_penalty=True, env_configuration=env_configuration,
                          render_mode=render_mode)
 
 
 class IntersectionWithGsEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration,  render_mode=None):
         super().__init__(is_intersection_map=True, match_angle_only=False,
                          incent_win=True, env_configuration=env_configuration,
                          render_mode=render_mode)
 
 
 class IntersectionWithGsAllowDecelEnv(Deepdrive2DEnv):
-    def __init__(self, env_configuration, render_mode):
+    def __init__(self, env_configuration, render_mode=None):
         super().__init__(is_intersection_map=True, match_angle_only=False,
                          incent_win=True, forbid_deceleration=False, env_configuration=env_configuration,
                          render_mode=render_mode)
